b3a63e16d389a821c89310b1a34b43ea28eedf7b
Mabende <belindamabende@gmail.com>
2024-06-15 16:11:18 +0200

    Initial Commit

---
+++ Card_Game_R11/Card_Game_R11App.swift
@@ -0,0 +1,17 @@
+//
+//  Card_Game_R11App.swift
+//  Card_Game_R11
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/06/15.
+//
+
+import SwiftUI
+
+@main
+struct Card_Game_R11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Card_Game_R11Tests/Card_Game_R11Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Card_Game_R11Tests.swift
+//  Card_Game_R11Tests
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/06/15.
+//
+
+import Testing
+@testable import Card_Game_R11
+
+struct Card_Game_R11Tests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
